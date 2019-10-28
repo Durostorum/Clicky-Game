fd17a6e1e3c44eb30623f93f90d1dd4ac4067d63
Georgi <georgi.nikolow@gmail.com>
2019-10-28 12:43:49 -0400

    Updates

---
+++ precache-manifest.0f2fc46559c0a590ca8cea0ce88a9daf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84de627b7841124079ce268740270534",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c75bfc74b88b0b24e66e",
+    "url": "/Clicky-Game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6bcb8f6331868d044c6f",
+    "url": "/Clicky-Game/static/js/2.d4deef7b.chunk.js"
+  },
+  {
+    "revision": "c75bfc74b88b0b24e66e",
+    "url": "/Clicky-Game/static/js/main.9781080f.chunk.js"
+  },
+  {
+    "revision": "079cbe7a033af5aaf7b2",
+    "url": "/Clicky-Game/static/js/runtime-main.e762c1e2.js"
+  }
+]);
